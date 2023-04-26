139c926c82e334901294a64b2f0af3889fba90d8
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2023-04-26 20:09:15 +0600

    Class Declaration

--- main.cpp
+++ main.cpp
@@ -1,1 +1,61 @@
+/*
 
+Author: Tahmid Muntaser......
+
+ATM Management System 1.0
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Screen
+{
+
+public:
+};
+
+class Keypad
+{
+
+public:
+};
+
+class CashDispenser
+{
+
+public:
+};
+
+class DepositSlot
+{
+
+public:
+};
+
+class BankDatabase
+{
+
+public:
+};
+
+class Deposit
+{
+
+public:
+};
+
+class ATMController
+{
+
+public:
+};
+
+int main(int argc, char const *argv[])
+{
+    ATMController atm;
+    atm.run();
+
+    return 0;
+}
